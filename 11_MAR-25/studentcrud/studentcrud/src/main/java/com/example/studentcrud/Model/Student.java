0300c85d54d616da3b0ab1ded2e21f8df7531386
Sudeepdn <sudeepnaduvinamani2002@gmail.com>
2025-03-13 15:59:38 +0530

    files uploded

---
+++ 11_MAR-25/studentcrud/studentcrud/src/main/java/com/example/studentcrud/Model/Student.java
@@ -0,0 +1,6 @@
+package main.java.com.example.studentcrud.model;
+
+
+public class Student {
+    
+}
--- 11_MAR-25/studentcrud/studentcrud/src/main/java/com/example/studentcrud/StudentcrudApplication.java
+++ 11_MAR-25/studentcrud/studentcrud/src/main/java/com/example/studentcrud/StudentcrudApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentcrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentcrudApplication.class, args);
+		SpringApplication.run(StudentcrudApplication.class,args);
 	}
 
 }
